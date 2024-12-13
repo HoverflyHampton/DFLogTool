27cfc43c9e2ea335b8b592e1ae2c12e46593ccb9
William Hampton <william.hampton@hoverflytech.com>
2024-12-13 11:09:18 -0500

    feat: added setup.py back in

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, find_packages
+
+setup(name='log_parser',
+      version='1.2.12',
+      description='Python DF Log Merge and Parsing Tool',
+      author='William Hampton',
+      packages=find_packages(),
+     )
